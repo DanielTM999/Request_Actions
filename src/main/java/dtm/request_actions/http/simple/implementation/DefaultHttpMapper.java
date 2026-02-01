fec1df797383e9e3340da7555a87628a149a8073
= <=>
2026-02-01 06:58:24 -0300

    .

--- src/main/java/dtm/request_actions/http/simple/implementation/DefaultHttpMapper.java
+++ src/main/java/dtm/request_actions/http/simple/implementation/DefaultHttpMapper.java
@@ -1,6 +1,7 @@
 package dtm.request_actions.http.simple.implementation;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
@@ -9,20 +10,26 @@ import dtm.request_actions.http.simple.core.mapper.HttpMapper;
 
 public class DefaultHttpMapper implements HttpMapper{
 
+    private static final ObjectMapper jsonMapper = new ObjectMapper();
+    private static final XmlMapper xmlMapper = new XmlMapper();
+
     @Override
     public <T> T mapper(String baseRequestResponse, Class<T> referenceMapper, HttpType httpType) {
-        if(referenceMapper.equals(String.class)){
+        if(String.class.equals(referenceMapper)){
             return referenceMapper.cast(baseRequestResponse);
         }else{
             try {
                 if(httpType.equals(HttpType.JSON)){
-                    ObjectMapper mapper = new ObjectMapper();
-                    return mapper.readValue(baseRequestResponse, referenceMapper);
+                    if(JsonNode.class.equals(referenceMapper)){
+                        return referenceMapper.cast(jsonMapper.readTree(baseRequestResponse));
+                    }
+
+                    return jsonMapper.readValue(baseRequestResponse, referenceMapper);
                 }else{
-                    XmlMapper xmlMapper = new XmlMapper();
                     return xmlMapper.readValue(baseRequestResponse, referenceMapper);
                 }
             } catch (Exception e) {
+                e.printStackTrace(System.out);
                return null;
             }
         }
@@ -30,20 +37,20 @@ public class DefaultHttpMapper implements HttpMapper{
 
     @Override
     public String mapperToJson(Object baseRequestResponse) {
-        ObjectMapper mapper = new ObjectMapper();
         try {
-            return mapper.writeValueAsString(baseRequestResponse);
+            return jsonMapper.writeValueAsString(baseRequestResponse);
         } catch (JsonProcessingException e) {
+            e.printStackTrace(System.out);
             return null;
         }
     }
 
     @Override
     public String mapperToXML(Object baseRequestResponse) {
-        XmlMapper xmlMapper = new XmlMapper();
         try {
             return xmlMapper.writeValueAsString(baseRequestResponse);
         } catch (JsonProcessingException e) {
+            e.printStackTrace(System.out);
             return null;
         }
     }
